36f5b82534c8290541e31842688d3a2ba6493920
Mr-jk03 <giangcuong0603@gmail.com>
2024-10-03 10:28:34 +0700

    update my portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import About from './component/About/About'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './component/Navbar/Navbar';
 import Onlineportfolio from './component/Onlineportfolio/Onlineportfolio';
 import Education from './component/Education/Education'
@@ -12,11 +12,11 @@ import Project from './component/Project/Project';
 function App() {
   return (
     <div className="App">
-      
+
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/onlineportfolio' element={<Onlineportfolio />}/>
+          <Route path='/onlineportfolio' element={<Onlineportfolio/>}/>
           <Route path='/about' element={<About />} />
           <Route path='/education' element={<Education />}/>
           <Route path='/experience' element={<Experience />}/>
